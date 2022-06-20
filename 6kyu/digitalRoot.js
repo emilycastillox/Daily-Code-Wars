5d566bb739fb85a9fae74a42203618144d574a12
Emily Castillo <emilycastillox@gmail.com>
2022-06-19 23:47:02 -0400

    digital root

---
+++ 6kyu/digitalRoot.js
@@ -0,0 +1,33 @@
+/* 
+
+Instructions:
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+P: a number/integer n
+R: the sume of all of digits in n - so a number - cannot be double digits -only single
+E: 26 // 8 ; 256 // 13 // 4
+P: split up each digit in the number
+add each digit
+if total is double digit or > 9, continue to split and then add digits
+
+
+*/
+
+function digital_root(n) {
+        if (n < 10)
+          return n;
+      
+        for (let sum = 0, i = 0, n = String(n); i < n.length; i++)
+          sum += Number(n[i]);
+         
+        return digital_root(sum);
+  }
